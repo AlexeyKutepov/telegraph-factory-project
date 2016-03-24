0f40615a28778600e6ac417eec282c10670af044
Alexey Kutepov <alkutepov@gmail.com>
2016-03-24 12:45:12 +0300

    Добавил в админку механизм подписки на новости

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from subscribe.models import Subscriber
+
+
+@admin.register(Subscriber)
+class Subscriber(admin.ModelAdmin):
+    list_display = ('email', )
+    list_filter = ('email', )

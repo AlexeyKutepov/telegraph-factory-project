b92bc36bdb4aa23a0aa8998d8387f1cd328f4111
Alexey Kutepov <alkutepov@gmail.com>
2016-03-28 16:43:16 +0300

    Добавил в отзыв адрес сайта

--- testimonial/models.py
+++ testimonial/models.py
@@ -7,11 +7,13 @@ from filer.fields.image import FilerImageField
 
 from django.db import models
 
+
 class Testimonial(CMSPlugin):
     photo = FilerImageField(blank=True, null=True)
     text = models.TextField()
     user_name = models.CharField(max_length=100)
     user_position = models.CharField(max_length=100)
+    site = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.user_name

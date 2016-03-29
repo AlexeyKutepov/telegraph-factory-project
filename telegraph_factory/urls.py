510af54c33537f14e74440fb851425485602f686
Alexey Kutepov <alkutepov@gmail.com>
2016-03-29 09:37:51 +0300

    Добавил robots.txt

--- telegraph_factory/urls.py
+++ telegraph_factory/urls.py
@@ -7,10 +7,12 @@ from django.conf.urls import *  # NOQA
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = i18n_patterns('',
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^subscribe/', include('subscribe.urls')),
     url(r'^callback/', include('callback.urls')),
     url(r'^admin/', include(admin.site.urls)),  # NOQA
